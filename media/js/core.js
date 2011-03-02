c6b504afd85467588efe3333ef8f541d8428e646
George Tsiokos <gtsiokos.fusion.design@gmail.com>
2011-03-02 14:08:19 +0200

    [Fixed] previous commit

--- media/js/core.js
+++ media/js/core.js
@@ -77,7 +77,8 @@ $(function(){
 				hidePanel();
 			} else {
 				showPanel();
-				options.find("input")[0].focus();
+				var firstInput = options.find("input")[0];
+				if (firstInput) { firstInput.focus(); }
 			}
 			if(e){ e.stopPropagation(); }
 			if(e){ e.preventDefault(); }

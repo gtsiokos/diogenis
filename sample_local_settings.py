986d39fd7d48d67b75466fcf3be250335cf78168
Stefanos Chrousis <tenebrarius@hotmail.com>
2011-03-21 21:59:22 +0200

    Changed settings file, moved some settings file variables @ local_settings, added sample_local_settings file

---
+++ sample_local_settings.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+
+# Sample configuration file for diogenis.teilar.gr website
+# Rename to local_settings.py and change accordingly the vars
+
+# Change to True for development boxes
+DEBUG = False
+TEMPLATE_DEBUG = DEBUG
+
+ADMINS = (
+    # ('Your Name', 'your_email@domain.com'),
+)
+
+# Change them to reflect your own db settings
+DATABASE_ENGINE = 'mysql'           # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+DATABASE_NAME = 'diogenis'             # Or path to database file if using sqlite3.
+DATABASE_USER = 'diogenis'             # Not used with sqlite3.
+DATABASE_PASSWORD = 'diogenis' # Not used with sqlite3.
+DATABASE_HOST = ''             # Set to empty string for localhost. Not used with sqlite3.
+DATABASE_PORT = ''             # Set to empty string for default. Not used with sqlite3.
+
+# Make this unique, and don't share it with anybody.
+SECRET_KEY = 'secret'
+
+# Change PROJECT_ROOT to reflect your own dir
+PROJECT_ROOT = '/home/USER/diogenis/'
+
+# Create a media/ dir for you own static content
+# and create an admin_media symlink pointing to django's media
+# (admin_media is used for django's admin panel static content)
+MEDIA_ROOT = PROJECT_ROOT + 'media/'
+MEDIA_URL = '/media/'
+ADMIN_MEDIA_PREFIX = '/admin_media/'
+
+
+# LDAP
+BIND_USER = 'uid=root,dc=teilar,dc=gr'
+BIND_PASSWORD = 'secret'
+
+##### The following variables can remain unset in development boxes
+
+
+# used to send mails by sendmail
+EMAIL_HOST_USER = 'root'
--- settings.py
+++ settings.py
@@ -1,31 +1,21 @@
+# -*- coding: utf-8 -*-
 # Django settings for diogenis project.
 
 import os.path
 
 try:
-	from local_settings import *
+	from diogenis.local_settings import *
 except:
 	pass
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
-ADMINS = (
-    # ('Your Name', 'your_email@domain.com'),
-)
-
 AUTH_PROFILE_MODULE = 'accounts.UserProfile'
 
 #SESSION_EXPIRE_AT_BROWSER_CLOSE = True
 
 MANAGERS = ADMINS
-
-DATABASE_ENGINE = 'django.db.backends.sqlite3'           									# 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-DATABASE_NAME = os.path.join(os.path.dirname(__file__), 'diogenis.db')		# Or path to database file if using sqlite3.
-DATABASE_USER = ''             # Not used with sqlite3.
-DATABASE_PASSWORD = ''         # Not used with sqlite3.
-DATABASE_HOST = ''             # Set to empty string for localhost. Not used with sqlite3.
-DATABASE_PORT = ''             # Set to empty string for default. Not used with sqlite3.
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
@@ -43,23 +33,6 @@ SITE_ID = 1
 # If you set this to False, Django will make some optimizations so as not
 # to load the internationalization machinery.
 USE_I18N = True
-
-# Absolute path to the directory that holds media.
-# Example: "/home/media/media.lawrence.com/"
-MEDIA_ROOT = os.path.join(os.path.dirname(__file__), 'media')
-
-# URL that handles the media served from MEDIA_ROOT. Make sure to use a
-# trailing slash if there is a path component (optional in other cases).
-# Examples: "http://media.lawrence.com", "http://example.com/media/"
-MEDIA_URL = 'http://127.0.0.1:8080/media/'
-
-# URL prefix for admin media -- CSS, JavaScript and images. Make sure to use a
-# trailing slash.
-# Examples: "http://foo.com/media/", "/media/".
-ADMIN_MEDIA_PREFIX = '/admin_media/'
-
-# Make this unique, and don't share it with anybody.
-SECRET_KEY = 'n$3ho=zlga1q5vjh83^_ji%flfqj)q#3%r1u90r_)j0#b@6%-%'
 
 # List of callables that know how to import templates from various sources.
 TEMPLATE_LOADERS = (
@@ -106,15 +79,6 @@ except:
 
 # Needed for the decorator
 LOGIN_URL = '/'
-
-# Needed for the custom user profile
-								##			WARNING!!! WARNING!!! WARNING!!! WARNING!!! WARNING!!! WARNING!!! 
-								##			WARNING!!! WARNING!!! WARNING!!! WARNING!!! WARNING!!! WARNING!!! 
-								##			WARNING!!! WARNING!!! WARNING!!! WARNING!!! WARNING!!! WARNING!!! 
-								##			WARNING!!! WARNING!!! WARNING!!! WARNING!!! WARNING!!! WARNING!!! 
-								##			WARNING!!! WARNING!!! WARNING!!! WARNING!!! WARNING!!! WARNING!!! 
-								## UPARXEI H IDIA METAVLHTH STHN ARXH TOU ARXEIOU ME TIMH TO PATH TOU PALIOY APP 'accounts' TOU DIOGENH.
-#AUTH_PROFILE_MODULE = 'user.LdapProfile'
 
 # LDAP
 LDAP_SERVER = 'localhost'
